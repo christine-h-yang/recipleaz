152ce12f5506ae06834ea35382f2451ba47b9924
Christine Yang <sporty.pal@gmail.com>
2018-04-18 18:05:29 -0500

    Added side navigation layout for user accounts

--- Recipleaz/app/src/main/java/cs371m/com/recipleaz/MainActivity.java
+++ Recipleaz/app/src/main/java/cs371m/com/recipleaz/MainActivity.java
@@ -8,8 +8,11 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected static final int READ_EXTERNAL_STORAGE_PERMISSION_CODE = 1;
     protected static final int PICK_FROM_GALLERY = 1;
 
+    private ActionBarDrawerToggle toggle;
     private Button searchButton;
     private EditText searchText;
     private ImageButton uploadPhoto;
@@ -35,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //updateUserDisplay();
+            }
+        };
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         searchButton = findViewById(R.id.search_button);
         searchText = findViewById(R.id.search_text);
